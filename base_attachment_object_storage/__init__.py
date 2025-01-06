47f0ea743b961e8784d86068a2a85f2fc028961d
vrenaville <vincent.renaville@camptocamp.com>
2025-01-06 14:43:59 +0100

    fix: instance definition

--- base_attachment_object_storage/__init__.py
+++ base_attachment_object_storage/__init__.py
@@ -1,26 +1,2 @@
 from . import models
-from odoo.http import Stream
 
-
-old_from_attachment = Stream.as_attachment
-
-
-@classmethod
-def from_attachment(cls, attachment):
-    if attachment.store_fname and attachment._is_file_from_a_store(
-        attachment.store_fname
-    ):
-        self = cls(
-            mimetype=attachment.mimetype,
-            download_name=attachment.name,
-            conditional=True,
-            etag=attachment.checksum,
-        )
-        self.type = "data"
-        self.data = attachment.raw
-        self.size = len(self.data)
-        return self
-    return old_from_attachment(attachment)
-
-
-Stream.as_attachment = from_attachment
